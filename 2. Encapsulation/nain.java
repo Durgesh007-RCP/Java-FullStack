f7ac43c0e90b76416755c7c2689eb42a207c6375
Durgesh Bhatu Patil <durgeshpatil588@gmail.com>
2026-08-17 09:47:24 +0530

    Add files via upload

---
+++ 2. Encapsulation/nain.java
@@ -0,0 +1,20 @@
+package Exception;
+
+import java.util.Scanner;
+
+public class nain {
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        //String s1=sc.next();
+
+
+        String s2=sc.nextLine();
+        String s1=sc.next();
+
+
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
